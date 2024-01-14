97fa0f3ba31bf6d9c79d8733dcbbb16d6051472b
ier24 <ryohr93@gmail.com>
2024-01-14 17:25:00 +0900

    feat: chat with gpt4

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
+import * as readline from "readline";
 
 const PROTO_PATH = "./protos/hello.proto";
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
@@ -17,10 +18,47 @@ const client = new GreeterService(
   grpc.credentials.createInsecure()
 );
 
+const ChatService = hello.ChatService;
+const chatClient = new ChatService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+const chat = chatClient.chat();
+
+// チャットを終了する
+setTimeout(() => {
+  chat.end();
+}, 115000);
+
 client.sayHello({ name: "Takashi!" }, (error: any, response: any) => {
   if (!error) {
     console.log("Greeting:", response.message);
   } else {
     console.error(error);
   }
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  if (line === "exit") {
+    chat.end();
+    rl.close();
+    console.log("Chat ended by user.");
+  } else {
+    chat.write({ message: line });
+  }
+});
+
+chat.on("data", (message: any) => {
+  console.log("Received reply:", message.message);
+});
+
+chat.on("end", () => {
+  rl.close();
+  console.log("Chat ended.");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
+import OpenAI from "openai";
 
 const PROTO_PATH = "./protos/hello.proto";
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
@@ -10,6 +11,38 @@ const helloWorldDescriptor = protoDescriptor.helloworld as any;
 server.addService(helloWorldDescriptor["Greeter"].service, {
   sayHello: (call: any, callback: any) => {
     callback(null, { message: "Hello " + call.request.name });
+  },
+});
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+server.addService(helloWorldDescriptor["ChatService"].service, {
+  chat: (call: any) => {
+    call.on("data", async (message: any) => {
+      console.log("Received message:", message.message);
+      const streamResponse = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "system",
+            content: "You are a helpful assistant. 必ず140字以内で返答して。",
+          },
+          { role: "user", content: `${message.message}` },
+        ],
+        stream: true,
+        model: "gpt-4-1106-preview",
+      });
+      let reply = "";
+      for await (const chunk of streamResponse) {
+        const content = chunk.choices[0]?.delta?.content || "";
+        reply += content;
+      }
+      console.log("Reply from AI:", reply);
+      call.write({ message: "Reply from AI: " + reply });
+    });
+
+    call.on("end", () => {
+      call.end();
+    });
   },
 });
 
